fff141093930297e2beb8a13055cd92ef931ed37
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2025-06-03 07:47:42 +0900

    Add more test patterns

--- benchmark/branch.rb
+++ benchmark/branch.rb
@@ -1,7 +1,6 @@
 require "benchmark/ips"
 require "uri/whatwg_parser"
 
-whatwg_parser = URI::WhatwgParser.new
 $VERBOSE = nil
 
 condition = `git name-rev --name-only HEAD`.strip
@@ -12,6 +11,14 @@ end
 Benchmark.ips do |x|
   x.report("parse #{condition}") do
     URI.parse("http://www.ruby-lang.org/")
+  end
+
+  x.report("parse(multibyte) #{condition}") do
+    URI.parse("http://日本語.jp")
+  end
+
+  x.report("parse(long uri) #{condition}") do
+    URI.parse("https://user:password@example.com/path/to/resource?query=string#fragment")
   end
 
   x.save!('/tmp/whatg_parse-benchmark')
