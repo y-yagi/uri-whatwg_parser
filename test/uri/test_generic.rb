fa74f5d16fd9c50b2773a58fa75e1cadb5cd1a4f
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2025-10-19 16:09:31 +0900

    Extract tests for basic methods to a file

---
+++ test/uri/test_generic.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class URI::TestGenericTest < Test::Unit::TestCase
+  def test_parse
+    uri = URI.parse("http://foo.com/posts?id=30&limit=5#time=1305298413")
+    assert_equal "http", uri.scheme
+    assert_equal "foo.com", uri.host
+    assert_equal 80, uri.port
+    assert_equal "/posts", uri.path
+    assert_equal "id=30&limit=5", uri.query
+    assert_equal "time=1305298413", uri.fragment
+  end
+
+  def test_join
+    uri = URI.join("http://www.ruby-lang.org/")
+    assert_equal uri.to_s, "http://www.ruby-lang.org/"
+
+    uri = URI.join("http://www.ruby-lang.org/", "/ja/man-1.6/")
+    assert_equal uri.to_s, "http://www.ruby-lang.org/ja/man-1.6/"
+
+    uri = URI.join("http://www.ruby-lang.org/", "/ja/man-1.6/" "b")
+    assert_equal uri.to_s, "http://www.ruby-lang.org/ja/man-1.6/b"
+  end
+end
