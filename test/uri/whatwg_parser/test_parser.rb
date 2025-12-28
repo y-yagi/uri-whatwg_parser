861c265e9a997e533b7d599947f3e67a5060d606
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2025-12-28 13:10:31 +0900

    Add a test case when encoding is specified

--- test/uri/whatwg_parser/test_parser.rb
+++ test/uri/whatwg_parser/test_parser.rb
@@ -11,4 +11,12 @@ class URI::WhatwgParser::TestParserTest < Test::Unit::TestCase
     sjis_encoded = parser.percent_encode('あ', [], Encoding::Shift_JIS)
     assert_equal '%82%A0', sjis_encoded
   end
+
+  def test_encoding
+    parser = URI::WhatwgParser.new
+    result = parser.split("dummy://example.com/?a=あ", encoding: Encoding::SJIS)
+    assert_equal "dummy", result[0]
+    assert_equal "example.com", result[2]
+    assert_equal "a=%E3%81%82", result[7]
+  end
 end
