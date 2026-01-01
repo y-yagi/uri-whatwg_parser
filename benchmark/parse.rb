0390540accef0d73c9ebec6a04bc08f185786170
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2026-01-01 09:18:06 +0900

    Adjust benchmark script

--- benchmark/parse.rb
+++ benchmark/parse.rb
@@ -3,19 +3,26 @@ require "uri/whatwg_parser"
 
 uri = "http://www.ruby-lang.org/"
 long_uri = "https://user:password@example.com/path/to/resource?query=string#fragment"
-whatwg_parser = URI::WhatwgParser.new
 $VERBOSE = nil
 
 Benchmark.ips do |x|
-  x.report("WHATWG")  do
-    URI::DEFAULT_PARSER = whatwg_parser
+  x.report("WHATWG parse")  do
+    URI::DEFAULT_PARSER = URI::WHATWG_PARSER
     URI.parse(uri)
+  end
+
+  x.report("WHATWG parse(long uri)")  do
+    URI::DEFAULT_PARSER = URI::WHATWG_PARSER
     URI.parse(long_uri)
   end
 
-  x.report("RFC3986") do
+  x.report("RFC3986 parse") do
     URI::DEFAULT_PARSER = URI::RFC3986_PARSER
     URI.parse(uri)
+  end
+
+  x.report("RFC3986 parse(long uri)") do
+    URI::DEFAULT_PARSER = URI::RFC3986_PARSER
     URI.parse(long_uri)
   end
 
